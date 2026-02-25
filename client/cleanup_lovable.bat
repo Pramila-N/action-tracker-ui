b56aa4fd8234c4545e0188c7da147d4456419ef8
Pramila-N <npramila2006@gmail.com>
2026-02-25 16:03:43 +0530

    Unnecessary things removed

--- client/cleanup_lovable.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "c:\Users\gokil\Downloads\action item tracker\action-tracker-ui\client"
-if exist package-lock.json del package-lock.json
-echo Removed package-lock.json
-echo.
-echo Now run: bun install (or npm install)
-echo to regenerate the lock file without lovable references
